95d8d113df46e213a968ae80955e1bd527a758b1
Keith C. Ivey <keith@iveys.org>
2019-03-18 22:34:15 -0400

    start parsing candidate list

--- get-races.js
+++ get-races.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
 const cheerio = require('cheerio');
+const _ = require('lodash');
 const request = require('./request');
 const houseUrl = 'https://www.vpap.org/elections/house/candidates/general/';
 const senateUrl = 'https://www.vpap.org/elections/senate/candidates/general/';
 
-request(houseUrl).then(() => request(senateUrl));
+request(houseUrl).then(processHtml);
 
 function processHtml(html) {
     const $ = cheerio.load(html);
-    $('table.table > tr')
+    const headers = $('table.table thead tr:first-child th')
+        .map((i, th) => $(th).text().trim()).get();
+    $('table.table tbody tr').each(function (i, tr) {
+        const values = $(tr).find('td').map((i, td) => $(td).html().trim().replace(/\s+/g, ' '))
+            .get();
+        const record = _.zipObject(headers, values);
+        console.log(record);
+    });
 }
