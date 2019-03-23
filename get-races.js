0d5d646dcebee7f44bcf6496fd1e32642cf4e1ea
Keith C. Ivey <keith@iveys.org>
2019-03-22 23:18:53 -0400

    get async/await working for retrieving detail URLs

--- get-races.js
+++ get-races.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const url = require('url');
 const cheerio = require('cheerio');
 const _ = require('lodash');
 const csvStringify = require('csv-stringify/lib/sync');
@@ -13,12 +14,13 @@ request(houseUrl).then(processHtml)
     .then(processHtml)
     .then(() => console.log(csvStringify(data, {header: true})));
 
-function processHtml(html) {
+async function processHtml(html) {
     const $ = cheerio.load(html);
     const headers = $('table.table thead tr:first-child th')
         .map((i, th) => $(th).text().trim()).get();
-    $('table.table tbody tr').each(function (i, tr) {
-        const values = $(tr).find('td')
+    const rows = $('table.table tbody tr').get();
+    for (const row of rows) {
+        const values = $(row).find('td')
             .map(function (i, td) {
                 const value = $(td).html().trim().replace(/\s+/g, ' ');
                 return i > 0 ? value.replace(/\s*<br>\s*/g, '; ').replace(/;\s*$/, '') : value;
@@ -26,10 +28,12 @@ function processHtml(html) {
             .get();
         let m;
         if ((m = values[0].match(/href="([^"]+)"\s*>\s*([HS]D\s*\d+)\s*</))) {
-            const detailUrl = m[1];
             values[0] = m[2].replace(/\s+/, '');
+            const detailUrl = url.resolve(houseUrl, m[1]);
+            const html = await request(detailUrl);
+            console.log(values[0]);
         }
         const record = _.zipObject(headers, values);
         data.push(record);
-    });
+    }
 }
