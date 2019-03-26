4e4bf209af4d02fb951f00dbb0dde62c38bf21e1
Keith C. Ivey <keith@iveys.org>
2019-03-26 13:15:38 -0400

    start getting more details for races

--- get-races.js
+++ get-races.js
@@ -18,8 +18,10 @@ request(houseUrl).then(processHtml)
 async function processHtml(html) {
     const $ = cheerio.load(html);
     const headers = $('table.table thead tr:first-child th')
-        .map((i, th) => $(th).text().trim()).get();
-    const rows = $('table.table tbody tr').get();
+        .map((i, th) => $(th).text().trim()).get()
+        .concat('Incumbent', 'Party');
+    let rows = $('table.table tbody tr').get();
+    rows = rows.slice(0, 4); // @@@
     for (const row of rows) {
         const values = $(row).find('td')
             .map(function (i, td) {
@@ -34,9 +36,17 @@ async function processHtml(html) {
         let m;
         if ((m = values[0].match(/href="([^"]+)"\s*>\s*([HS]D\s*\d+)\s*</))) {
             values[0] = m[2].replace(/\s+/, '');
-            const detailUrl = url.resolve(houseUrl, m[1]);
-            // const html = await request(detailUrl);
-            console.log(values[0]);
+            const detailUrl = url.resolve(houseUrl, m[1])
+                .replace(/\/elections\/.*/, '/district/');
+            const html = await request(detailUrl);
+            const $ = cheerio.load(html);
+            let incumbent = $('div.col-xs-12.col-md-3').find('div').eq(1).text().trim();
+            let party = '';
+            if ((m = incumbent.match(/^([^,+]+),\s+(.+?)\s+(\w+)$/))) {
+                incumbent = m[2] + ' ' + m[1];
+                party = m[3].substr(0, 1);
+            }
+            values.push(incumbent, party);
         }
         const record = _.zipObject(headers, values);
         data.push(record);
