bd837d6ee95cc2841d2515301666ce2972bfa5e6
Keith C. Ivey <keith@iveys.org>
2019-03-27 23:47:03 -0400

    get vote counts for house races

--- get-races.js
+++ get-races.js
@@ -19,7 +19,7 @@ async function processHtml(html) {
     const $ = cheerio.load(html);
     const headers = $('table.table thead tr:first-child th')
         .map((i, th) => $(th).text().trim()).get()
-        .concat('Incumbent', 'Party');
+        .concat('D Votes', 'R Votes', 'Incumbent', 'Party');
     let rows = $('table.table tbody tr').get();
     rows = rows.slice(0, 4); // @@@
     for (const row of rows) {
@@ -36,10 +36,37 @@ async function processHtml(html) {
         let m;
         if ((m = values[0].match(/href="([^"]+)"\s*>\s*([HS]D\s*\d+)\s*</))) {
             values[0] = m[2].replace(/\s+/, '');
-            const detailUrl = url.resolve(houseUrl, m[1])
-                .replace(/\/elections\/.*/, '/district/');
-            const html = await request(detailUrl);
-            const $ = cheerio.load(html);
+            let detailUrl = url.resolve(houseUrl, m[1]);
+            const isHouse = /house/.test(detailUrl);
+            let html;
+            let $;
+            if (isHouse) {
+                detailUrl = detailUrl.replace('2019', '2017');
+                html = await request(detailUrl);
+                $ = cheerio.load(html);
+                rows = $('table.table tbody tr').get();
+                const votes = {};
+                for (const row of rows) {
+                    if ($(row).hasClass('ghost')) {
+                        continue;
+                    }
+                    const cells = $(row).find('td');
+                    const candidate = cells.eq(0).text();
+                    let m;
+                    if ((m = candidate.match(/\((\w)\)/))) {
+                        const party = m[1];
+                        votes[party] = cells.eq(2).text().replace(/^\s*([\d,]+)\s.+$/sm, '$1')
+                            .replace(',', '');
+                    }
+                }
+                values.push(votes['D'], votes['R']);
+            }
+            else {
+                values.push('', '');
+            }
+            detailUrl = detailUrl.replace(/\/elections\/.*/, '/district/');
+            html = await request(detailUrl);
+            $ = cheerio.load(html);
             let incumbent = $('div.col-xs-12.col-md-3').find('div').eq(1).text().trim();
             let party = '';
             if ((m = incumbent.match(/^([^,+]+),\s+(.+?)\s+(\w+)$/))) {
