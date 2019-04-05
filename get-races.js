e9121d9106af2056aa7e6950a066a51315f4849d
Keith C. Ivey <keith@iveys.org>
2019-04-05 18:49:46 -0400

    save data on races between runs and add command-line option

--- get-races.js
+++ get-races.js
@@ -1,112 +1,146 @@
 #!/usr/bin/env node
 
 const url = require('url');
+const fs = require('fs');
 const cheerio = require('cheerio');
 const _ = require('lodash');
+const program = require('commander');
+const yaml = require('js-yaml');
 const csvStringify = require('csv-stringify/lib/sync');
 const {decodeHTML} = require('entities');
 const request = require('./request');
 const houseUrl = 'https://www.vpap.org/elections/house/candidates/general/';
 const senateUrl = 'https://www.vpap.org/elections/senate/candidates/general/';
-const data = [];
+const dataFile = __dirname + '/races.yaml';
 const currentElectionYear = 2019;
+let data = {};
 
-request(houseUrl).then(processHtml)
+program.option('--full', 'Refresh all info, including historical vote counts (takes a while)')
+    .parse(process.argv);
+
+readExistingData()
+    .then(() => request(houseUrl))
+    .then(processHtml)
     .then(() => request(senateUrl))
     .then(processHtml)
+    .then(writeData)
     .then(outputCsv);
+
+function readExistingData() {
+    return new Promise(function (resolve, reject) {
+        if (program.full) {
+            resolve();
+            return;
+        }
+        fs.readFile(dataFile, 'utf8', function (err, yamlString) {
+            if (!err) {
+                data = yaml.safeLoad(yamlString);
+            }
+            else if (err.code !== 'ENOENT') {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
 
 async function processHtml(html) {
     let $ = cheerio.load(html);
     const headers = $('table.table thead tr:first-child th')
         .map((i, th) => $(th).text().trim())
         .get();
-    headers.unshift('Chamber');
+    headers.shift(); // remove "District" column head
     let rows = $('table.table tbody tr').get();
     for (const row of rows) {
-        const values = $(row).find('td')
-            .map(function (i, td) {
-                const value = $(td).html().trim().replace(/\s+/g, ' ');
-                return i === 0 ? value :
-                    decodeHTML(
-                        value.replace(/\s*<br>\s*/g, '; ')
-                            .replace(/;\s*$/, '')
-                    );
-            })
+        let values = $(row).find('td')
+            .map((i, td) => $(td).html().trim().replace(/\s+/g, ' '))
             .get();
-        let m = values[0].match(/href="([^"]+)"\s*>\s*([HS])D\s*(\d+)\s*</);
+        let district = values.shift();
+        values = values.map(function (value) {
+            return value.split(/\s*<br>\s*/g)
+                .filter(v => (v !== ''));
+        });
+        let m = district.match(/href="([^"]+)"\s*>\s*([HS]D\s*\d+)\s*</);
         if (!m) {
-            throw new Error(`Unexpected format "${values[0]}"`);
+            throw new Error(`Unexpected format "${district}"`);
         }
         let detailUrl = url.resolve(houseUrl, m[1]);
-        const isHouse = (m[2] === 'H');
-        values[0]= m[3]; // district number
-        values.unshift(m[2]); // chamber
+        district= m[2].replace(/\s+/, '');
         const record = _.zipObject(headers, values);
-        detailUrl = detailUrl.replace('2019', isHouse ? '2017' : '2015');
-        $ = await getCheerio(detailUrl);
-        rows = $('table.table tbody tr').get();
-        const votes = {};
-        for (const row of rows) {
-            if ($(row).hasClass('ghost')) {
-                continue;
+        if (program.full || !data[district]) {
+            const isHouse = /^H/.test(district);
+            detailUrl = detailUrl.replace('2019', isHouse ? '2017' : '2015');
+            $ = await getCheerio(detailUrl);
+            rows = $('table.table tbody tr').get();
+            const votes = {};
+            for (const row of rows) {
+                if ($(row).hasClass('ghost')) {
+                    continue;
+                }
+                const cells = $(row).find('td');
+                const candidate = cells.eq(0).text();
+                if ((m = candidate.match(/\((\w)\)/))) {
+                    votes[m[1]] = +cells.eq(2).text().replace(/^\s*([\d,]+)\s.+$/sm, '$1')
+                        .replace(/,/g, '');
+                }
             }
-            const cells = $(row).find('td');
-            const candidate = cells.eq(0).text();
-            if ((m = candidate.match(/\((\w)\)/))) {
-                votes[m[1]] = +cells.eq(2).text().replace(/^\s*([\d,]+)\s.+$/sm, '$1')
-                    .replace(/,/g, '');
+            for (const abbr of ['D', 'R']) {
+                record[`Previous ${abbr} Votes`] = votes[abbr] || 0;
             }
-        }
-        for (const abbr of ['D', 'R']) {
-            record[`Previous ${abbr} Votes`] = votes[abbr] || 0;
-        }
-        record['Previous Margin'] = getDemMargin(votes['D'], votes['R']);
-        detailUrl = detailUrl.replace(/\/elections\/.*/, '/district/');
-        $ = await getCheerio(detailUrl);
-        let incumbent = $('div.col-xs-12.col-md-3').find('div').eq(1).text().trim();
-        let party = '';
-        m = incumbent.match(/^([^,+]+),\s+(.+?)\s+(\w+)$/);
-        if (m) {
-            incumbent = m[2] + ' ' + m[1];
-            party = m[3].substr(0, 1);
-        }
-        record['Incumbent'] = incumbent;
-        record['Party'] = party;
-        const links = $('table.table-condensed a').get();
-        let prevYear = currentElectionYear;
-        for (const link of links) {
-            const $link = $(link);
-            const election = $link.text().trim();
-            m = election.match(/\b(20\d\d)\b/);
+            record['Previous Margin'] = getDemMargin(votes['D'], votes['R']);
+            detailUrl = detailUrl.replace(/\/elections\/.*/, '/district/');
+            $ = await getCheerio(detailUrl);
+            let incumbent = $('div.col-xs-12.col-md-3').find('div').eq(1).text().trim();
+            let party = '';
+            m = incumbent.match(/^([^,+]+),\s+(.+?)\s+(\w+)$/);
             if (m) {
-                const electionYear = +m[1];
-                if (electionYear < currentElectionYear - 5) {
-                    break;
-                }
-                else if (electionYear < prevYear) {
-                    const electionUrl = url.resolve(detailUrl, $link.attr('href'));
-                    $ = await getCheerio(electionUrl);
-                    const votes = {};
-                    const rows = $('table.top-buffer tbody tr').get();
-                    for (const row of rows) {
-                        const cells = $(row).find('td');
-                        m = cells.eq(0).text().match(/\(([^)]+)\)/);
-                        if (!m) {
-                            throw new Error(`Unexpected format "${cells.eq(0).text()}"`);
+                incumbent = m[2] + ' ' + m[1];
+                party = m[3].substr(0, 1);
+            }
+            record['Incumbent'] = incumbent;
+            record['Party'] = party;
+            const links = $('table.table-condensed a').get();
+            let prevYear = currentElectionYear;
+            for (const link of links) {
+                const $link = $(link);
+                const election = $link.text().trim();
+                m = election.match(/\b(20\d\d)\b/);
+                if (m) {
+                    const electionYear = +m[1];
+                    if (electionYear < currentElectionYear - 5) {
+                        break;
+                    }
+                    else if (electionYear < prevYear) {
+                        const electionUrl = url.resolve(detailUrl, $link.attr('href'));
+                        $ = await getCheerio(electionUrl);
+                        const votes = {};
+                        const rows = $('table.top-buffer tbody tr').get();
+                        for (const row of rows) {
+                            const cells = $(row).find('td');
+                            m = cells.eq(0).text().match(/\(([^)]+)\)/);
+                            if (!m) {
+                                throw new Error(`Unexpected format "${cells.eq(0).text()}"`);
+                            }
+                            votes[m[1]] = +cells.eq(1).text().trim()
+                                .replace(/,/g, '');
                         }
-                        votes[m[1]] = +cells.eq(1).text().trim()
-                            .replace(/,/g, '');
+                        for (const abbr of ['D', 'R']) {
+                            record[`${election} ${abbr}`] = votes[abbr] || 0;
+                        }
+                        record[`${election} Margin`] = getDemMargin(votes['D'], votes['R']);
                     }
-                    for (const abbr of ['D', 'R']) {
-                        record[`${election} ${abbr}`] = votes[abbr] || 0;
-                    }
-                    record[`${election} Margin`] = getDemMargin(votes['D'], votes['R']);
+                    prevYear = electionYear;
                 }
-                prevYear = electionYear;
             }
         }
-        data.push(record);
+        if (data[district]) {
+            for (const [key, value] of Object.entries(record)) {
+                data[district][key] = value;
+            }
+        }
+        else {
+            data[district] = record;
+        }
     }
     return data;
 }
@@ -122,6 +156,30 @@ function getDemMargin(dVotes, rVotes) {
     return (dVotes >= rVotes ? '+' : '') + (100 * (dVotes - rVotes) / (dVotes + rVotes)).toFixed(0);
 }
 
+function writeData() {
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(dataFile, yaml.safeDump(data), function (err) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
+
 function outputCsv(data) {
-    process.stdout.write(csvStringify(data, {header: true}));
+    const transformedData = [];
+    for (const [district, r] of Object.entries(data)) {
+        const record = {
+            Chamber: district.substr(0, 1),
+            District: +district.substr(2),
+        };
+        for (const [key, value] of Object.entries(r)) {
+            record[key] = Array.isArray(value) ? value.join('; ') : value;
+        }
+        transformedData.push(record);
+    }
+    process.stdout.write(csvStringify(transformedData, {header: true}));
 }
